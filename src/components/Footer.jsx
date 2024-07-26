a5656ffbd06bf8b0b16ff1f4653bca497e25aa8b
deepakdk11 <deepakkumar545424@gmail.com>
2024-07-26 16:34:31 +0530

    Fix some bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='p-4 border-t-2 dark:bg-black dark:text-white dark:border-gray-700'>
-      <h1 className='text-center'>Made with <span class="animate-pulse"> ❤️ </span> By <span className='text-yellow-600'>Deepak</span></h1>
+      <h1 className='text-center'>Made with <span className="animate-pulse"> ❤️ </span> By <span className='text-yellow-600'>Deepak</span></h1>
     </div>
   )
 }
